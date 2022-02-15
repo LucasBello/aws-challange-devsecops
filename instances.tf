28014006748abe2bfd85ee6d253f526c11deabbb
thiagoalmeidadon <thiagodons@gmail.com>
2022-02-15 17:22:30 -0300

    organizando as variaveis

--- instances.tf
+++ instances.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "challenge_server" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
-  key_name = "aws-geral"
+  ami = var.amis["us-east-1"]
+  provider = aws.us-east-1
+  instance_type = var.instance_type
+  key_name = var.key_name
   vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.jenkins-sg.id}"]
   tags = {
     Name = "Challenge_server"
